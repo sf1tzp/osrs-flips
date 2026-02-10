2a05697d232331a73700d9dd4f78e4dcff7c4fcd
Steven Fitzpatrick <steven@fitz.gg>
2026-02-10 05:18:18 +0000

    Add mode switcher

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -2,8 +2,11 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+	import { ModeWatcher } from "mode-watcher";
+
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
 {@render children()}
